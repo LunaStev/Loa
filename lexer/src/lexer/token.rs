a16b5d11bac6a0d8ef4771652b1035820195b45d
LunaStev <youngjae681@gmail.com>
2025-04-27 18:46:03 +0900

    Token modification

--- lexer/src/lexer/token.rs
+++ lexer/src/lexer/token.rs
@@ -1,81 +1,46 @@
+use std::fmt::Debug;
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum TokenType {
     Fun,
-    Var,
-    Let,
-    Mut,
-    Deref,
-    Const,
     If,
     Else,
     While,
     For,
-    Import,
     Return,
     Continue,
-    Input,
+    Break,
     Print,
     Println,
-    Module,
-    Class,
-    Match,
-    LogicalAnd,            // &&
-    AddressOf,            // &
-    LogicalOr,             // ||
-    BitwiseOr,             // |
-    NotEqual,              // !=
-    Xor,                    // ^
-    Xnor,                   // ~^
-    BitwiseNot,            // ~
-    Nand,                   // !&
-    Nor,                    // !|
-    Not,                    // !
-    Condition,              // ?
-    NullCoalesce,          // ??
-    Conditional,            // ?:
-    In,                     // in
-    Is,                     // is
-    Rol,
-    Ror,
-    Xnand,
-    Operator(String),
-    TypeInt(u16),
-    TypeFloat(u16),
-    TypeBool,
-    TypeChar,
-    TypeByte,
-    TypeString,
-    TypeArray(Box<TokenType>, u32),
+    Input,
+
     Identifier(String),
     String(String),
     Number(i64),
     Float(f64),
-    Plus,                   // +
-    Increment,              // ++
-    Minus,                  // -
-    Decrement,              // --
-    Star,                   // *
-    Div,                    // /
-    Equal,                  // =
-    EqualTwo,              // ==
-    Comma,                  // ,
-    Dot,                    // .
-    SemiColon,              // ;
-    Colon,                  // :
-    Lchevr,                 // <
-    LchevrEq,              // <=
-    Rchevr,                 // >
-    RchevrEq,              // >=
-    Lparen,                 // (
-    Rparen,                 // )
-    Lbrace,                 // {
-    Rbrace,                 // }
-    Lbrack,                 // [
-    Rbrack,                 // ]
-    Eof,                    // End of file
+
+    Plus,         // +
+    Minus,        // -
+    Star,         // *
+    Div,          // /
+    Equal,        // =
+    EqualTwo,     // ==
+    NotEqual,     // !=
+    Greater,      // >
+    GreaterEqual, // >=
+    Less,         // <
+    LessEqual,    // <=
+
+    LogicalAnd,   // &&
+    LogicalOr,    // ||
+    Not,          // !
+
+    Lparen,       // (
+    Rparen,       // )
+    Colon,        // :
+    Comma,        // ,
+
+    Eof,
     Error,
     Whitespace,
-    Break,
-    Arrow,                  // ->
-    Array,
 }
